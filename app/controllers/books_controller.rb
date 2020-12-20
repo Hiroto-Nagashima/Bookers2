e58cbc9c9747f4afba26e6c2b08a745f16662e22
EC2 Default User <ec2-user@ip-172-31-23-252.ec2.internal>
2020-12-20 06:59:03 +0000

    [bookers2]step2/add

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,16 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show]
+  get '/users' , to: 'users#index'
   root to: 'homes#top'
   get 'home/about' , to:'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-end 
+
+
+end
