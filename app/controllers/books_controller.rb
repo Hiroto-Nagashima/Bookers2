948863191d9a176cf53cbbaa4d3cca15d77c3f79
Hiroto-Nagashima <nh35813219254@gmail.com>
2020-12-25 13:34:34 +0000

    [bookers2]error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,18 +9,19 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @book_new= Book.new
-    
+
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
+    @user = current_user
     @book.user_id = current_user.id
-    
     if @book.save
       flash[:notice] ="You have created book successfully"
       redirect_to book_path(@book.id)
     else
-      render "show"
+      render "index"
     end
 
   end
@@ -29,15 +30,16 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.user_id = current_user.id
     book.destroy
-    redirect_to user_path(params[:id])
+    redirect_to books_path
   end
 
   def edit
-    @Book= Book.find(params[:id])
+    @book= Book.find(params[:id])
   end
 
   def update
-    if @book.update
+    @book= Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] ="You have updated book successfully"
       redirect_to user_path(current_user.id)
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   get 'home/about' , to:'homes#about'
-  resources :books, only: [:new, :create, :index, :show, :destroy,:edit]
+  resources :books, only: [:update, :create, :index, :show, :destroy,:edit]
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
