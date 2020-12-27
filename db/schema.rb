6a350d07c3c93c506b4e59c164f36aa433719b45
Hiroto-Nagashima <nh35813219254@gmail.com>
2020-12-27 12:55:59 +0000

    [bookers2]update

--- db/schema.rb
+++ db/schema.rb
@@ -21,12 +21,12 @@ ActiveRecord::Schema.define(version: 2020_12_20_040711) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "name"
     t.string "encrypted_password", default: "", null: false
+    t.string "email", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
     t.text "introduction"
     t.string "profile_image_id"
     t.datetime "created_at", null: false
